72e54288b6f945e7809d307e5fbf6bf11a503420
Rishabh6353 <rishabhoop@gmail.com>
2025-07-11 17:08:19 +0530

    Tries basics

---
+++ Tries/Basics.java
@@ -0,0 +1,53 @@
+public class Basics{
+
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        public Node(){
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word){
+        Node curr = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static boolean search(String word){
+        Node curr = root;
+
+        for(int i =0 ; i < word.length(); i++){
+            int idx = word.charAt(i) - 'a';
+
+            if(curr.children[idx] == null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+    }
+
+
+    public static void main(String[] args) {
+        String words[] = {"the", "a", "there", "their", "any", "thee"};
+        for (String word : words) {
+            insert(word);
+        }
+
+        System.out.println(search("thor"));
+        System.out.println(search("thee"));
+    }
+}
